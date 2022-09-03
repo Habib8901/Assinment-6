1ae6f46cfa297808c38107f05ca83a62d7eb4454
Habib8901 <ahhabib357@gmail.com>
2022-09-03 23:48:46 +0600

    all web data

---
+++ jason/loader.js
@@ -0,0 +1,31 @@
+var loader = document.getElementById('preloader');
+window.addEventListener('load', function(){
+    loader.style.display = 'none';
+    
+})
+console.log('loader')
+
+
+/*var loader;
+
+function loader(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.05);
+        }, 50);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loader = document.getElementById('loader');
+    loadNow(1);
+})
+*/
